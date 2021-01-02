5781780b9fe6d90592b849a493dcce70a38acc21
Viveksharma48b <viveksharma48b@gmail.com>
2021-01-03 00:57:20 +0530

    Home page Improvements

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let read = document.querySelector(":root");
-function get() {
-    // Get the styles (properties and values) for the root
-    let rs = getComputedStyle(read);
-    // Alert the value of the --blue variable
-    alert("The value of --state is: " + rs.getPropertyValue('--state'));
-  }
-function setAnimation(){
-    read.style.setProperty("--state","paused");
-}
+const app = document.getElementById('app');
 
-setTimeout(function (){
-    read.style.setProperty("--state","paused");},20000);
+const typewriter = new Typewriter(app, {
+  strings: ["C++/14", "Java"],
+  loop: true
+});
+typewriter
+    .typeString('I Know C++/14').pauseFor(1000).deleteChars(6)
+    .typeString('Java').pauseFor(1000).deleteChars(4)
+    .typeString("Linux").pauseFor(1000).deleteChars(5)
+    .typeString("JS").pauseFor(1000).deleteChars(2)
+    .typeString("HTML5").pauseFor(1000).deleteChars(5)
+    .typeString("CSS").pauseFor(1000).deleteChars(3)
+    .typeString("Git").pauseFor(1000).deleteAll()
+    .typeString("And most of all").pauseFor(1000).deleteAll()
+    .typeString("I like solving problems").pauseFor(1000)
+    .start();
